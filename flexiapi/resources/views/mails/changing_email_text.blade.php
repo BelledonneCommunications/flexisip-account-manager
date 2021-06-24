3447080f97458e4f838c7e2713bc5f6add2e0fa6
Timothée Jaussoin <timothee.jaussoin@belledonne-communications.com>
2021-06-24 10:28:29 +0200

    Fix email change link in the text version of the email

--- flexiapi/resources/views/mails/changing_email_text.blade.php
+++ flexiapi/resources/views/mails/changing_email_text.blade.php
@@ -2,7 +2,7 @@ Hello,
 
 You requested to change your email address from {{ $account->emailChanged->new_email }} on {{ config('app.name') }}.
 
-To confirm this change please click on the following link: {{ route('account.email.request_update', $account->emailChanged->hash) }}.
+To confirm this change please click on the following link: {{ route('account.email.update', ['hash' => $account->emailChanged->hash]) }}.
 
 If you are not at the origin of this change just ignore this message.
 
