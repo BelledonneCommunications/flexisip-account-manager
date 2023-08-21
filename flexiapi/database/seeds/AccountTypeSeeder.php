3852218558da52137f9df7e36105143e3be335fd
Peio Rigaux <peio.rigaux@belledonne-communications.com>
2023-08-21 08:02:19 +0000

    Uses updateOrCreate instead of just create

--- flexiapi/database/seeds/AccountTypeSeeder.php
+++ flexiapi/database/seeds/AccountTypeSeeder.php
@@ -9,9 +9,9 @@ class AccountTypeSeeder extends Seeder
 {
     public function run()
     {
-        AccountType::create(['key' => 'device_audio_intercom']);
-        AccountType::create(['key' => 'device_video_intercom']);
-        AccountType::create(['key' => 'device_security_camera']);
-        AccountType::create(['key' => 'device_internal_unit']);
+        AccountType::updateOrCreate(['key' => 'device_audio_intercom']);
+        AccountType::updateOrCreate(['key' => 'device_video_intercom']);
+        AccountType::updateOrCreate(['key' => 'device_security_camera']);
+        AccountType::updateOrCreate(['key' => 'device_internal_unit']);
     }
 }
