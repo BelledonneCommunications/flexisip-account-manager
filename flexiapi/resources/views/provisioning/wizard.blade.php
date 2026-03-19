594a3d65eb30520725a7bee7468f6cab6f03c093
Timothée Jaussoin <timothee.jaussoin@belledonne-communications.com>
2026-03-19 16:01:06 +0100

    Fix FLEXIAPI-461 Detect the platform family and propose a button adapted for it
    Fix FLEXIAPI-463 Add wizard link in all the emails where the account is injected
    Update the dependencies

--- flexiapi/resources/views/provisioning/wizard.blade.php
+++ flexiapi/resources/views/provisioning/wizard.blade.php
@@ -7,8 +7,29 @@
     <a class="btn" href="linphone-config:{{ route('provisioning.provision', ['provisioning_token' => $token]) }}">
         {{ __('Open the app') }}
     </a>
-    <a class="btn secondary" target="_blank" href="https://www.linphone.org/en/download/">
-        {{ __('Download the app') }}
-    </a>
+    @if ($platform == 'GNU/Linux')
+        <a class="btn secondary" target="_blank" href="https://download.linphone.org/releases/linux/latest_app">
+            {{ __('Download Linphone for GNU/Linux') }}
+        </a>
+    @elseif ($platform == 'Mac')
+        <a class="btn secondary" target="_blank" href="https://download.linphone.org/releases/macos/latest_app">
+            {{ __('Download Linphone for MacOS') }}
+        </a>
+    @elseif ($platform == 'Windows')
+        <a class="btn secondary" target="_blank" href="https://download.linphone.org/releases/windows/latest_app">
+            {{ __('Download Linphone for Windows') }}
+        </a>
+    @else
+        <a class="btn secondary" target="_blank" href="https://www.linphone.org/en/download/">
+            {{ __('Download Linphone') }}
+        </a>
+    @endif
+    @if (in_array($platform, ['GNU/Linux', 'MacOS', 'Windows']))
+        <p class="center">
+            <a target="_blank" href="https://www.linphone.org/en/download/">
+                {{ __('Download for another platform') }}
+            </a>
+        </p>
+    @endif
 </div>
 @endsection
